986ad142f4d855e54bf932668a5959ebd42b5dfe
PizzaChips <jaroslav.vehovsky@gmail.com>
2022-10-18 13:49:14 +0200

    It's working somehow

--- t9search.c
+++ t9search.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <ctype.h>
 
+const char t9[10][5] = { "0",    "1",    "2abc",  "3def", "4ghi", "5jkl", "6mno", "7pqrs", "8tuv", "9wxyz" };
+
 // Checks if string is a number
 int isNumber(char str[]) {
     int len = strlen(str);
@@ -31,10 +33,12 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
-    char line[101];
-    char c;
+    char c, line[101];
     int index = 0;
     while ((c = getchar()) != EOF) {
+        // TODO: Everything to lower
+        // TODO: join into one line
+        // TODO: if number is not numbers ignore???
         if (index < 101) {
             line[index++] = c;
         } else {
@@ -42,7 +46,9 @@ int main(int argc, char *argv[]) {
         }
 
         if (c == '\n') {
-            // TODO: if empty replace with [empty]
+            if (strlen(line) < 3) {
+                strcpy(line, "[empty]\n");
+            }
             printf("%s", line);
             index = 0;
             memset(line, 0, 101);
