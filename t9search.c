184fa34671822fda5e86f41c0957a8f102b5ae50
PizzaChips <jaroslav.vehovsky@gmail.com>
2022-10-18 14:44:10 +0200

    Everything is small now

--- t9search.c
+++ t9search.c
@@ -15,10 +15,13 @@ int isNumber(char str[]) {
     return 1;
 }
 
-// TODO: If no contact found print "Not Found"
-// TODO: If results then print "Kontakt(y) nalezen(y)" and list the matching contacts in format "JMENO, TELCISLO"
-// TODO: (nepovinné, avšak může zlepšit hodnocení) Hledání přerušených posloupností znaků. Program najde i takové kontakty, u který mezi každými dvěma nalezenými znaky může být libovolná posloupnost jiných znaků. Důležité je, že nalezený řetězec (jména nebo telefonního čísla) musí obsahovat všechny znaky, které číslovky reprezentují, a ve stejném pořadí.
-// TODO: (prémiové) Hledání podobných kontaktů. Pokud zadal uživatel špatný vyhledávací řetězec (spletl se v čísle), program mu nabídne podobné výsledky. Uvažujte následující chyby uživatele: chybná číslice, chybějící číslice, číslice zadaná navíc. Program by měl být konfigurován argumentem programu -l L, kde L reprezentuje maximální počet uvažovaných chyb uživatele (pozn. symbol L je inspirován z tzv. Levenshteinovy vzdálenosti dvou řetězců).
+char toLowerCase(char x) {
+    if (x >= 'A' && x <= 'Z') {
+        x += 32;
+    }
+    return x;
+}
+
 int main(int argc, char *argv[]) {
     if (argc > 2) {
         fprintf(stderr, "Too many arguments\n");
@@ -36,17 +39,16 @@ int main(int argc, char *argv[]) {
     char c, line[101];
     int index = 0;
     while ((c = getchar()) != EOF) {
-        // TODO: Everything to lower
         // TODO: join into one line
         // TODO: if number is not numbers ignore???
         if (index < 101) {
-            line[index++] = c;
+            line[index++] = toLowerCase(c);
         } else {
             line[100] = '\n';
         }
 
         if (c == '\n') {
-            if (strlen(line) < 3) {
+            if (strlen(line) < 2) {
                 strcpy(line, "[empty]\n");
             }
             printf("%s", line);
