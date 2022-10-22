fbbcbc1bd6856bb3c6ff012a68112fe13ef4b445
PizzaChips <jaroslav.vehovsky@gmail.com>
2022-10-23 00:08:36 +0200

    Added comments

--- t9search.c
+++ t9search.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdlib.h>
 
-const int LINE_SIZE = 101;
-const int CONTACT_SIZE = 203;
-const char dictionary[10][4] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+const int LINE_SIZE = 101;      // Max length of name and number
+const int CONTACT_SIZE = 203;   // Max length of one line [name, number]
+const char dictionary[10][4] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}; // T9 dictionary
 
-// Checks if string is a number
+// isNumber checks if string 'str' is a number. Returns true or false
 int isNumber(char str[]) {
     int len = strlen(str);
     for (int i = 0; i < len; ++i) {
@@ -17,13 +18,12 @@ int isNumber(char str[]) {
     return 1;
 }
 
+// toLowerCase converts character 'x' to lowercase
 char toLowerCase(char x) {
-    if (x >= 'A' && x <= 'Z') {
-        x += 32;
-    }
-    return x;
+    return (x >= 'A' && x <= 'Z') ? x += 32 : x;
 }
 
+// charToNum converts character 'c' into a corresponding number from t9 dictionary
 char charToNum(char c) {
     for (int i = 0; i < 10; ++i) {
         for (int j = 0; j < (int)strlen(dictionary[i]); ++j) {
@@ -35,18 +35,23 @@ char charToNum(char c) {
     return c;
 }
 
+// checkMatch checks if contact 'contact' matches search query 'query'. Returns true or false
 int checkMatch(char contact[], char query[]) {
+    // Creates temporary variable and stores value of 'contact' in it
     char temp[strlen(contact)];
     strcpy(temp, contact);
 
+    // If no query was provided prints out all contacts
     if (strlen(query) == 0) {
         return 1;
     }
 
+    // Converts contact 'temp' into numbers
     for (int i = 0; i < (int)strlen(temp); ++i) {
         temp[i] = charToNum(temp[i]);
     }
 
+    // Checks for match and returns true if match is found
     if (strstr(temp, query)) {
         memset(temp, 0, strlen(contact));
         return 1;
@@ -57,15 +62,14 @@ int checkMatch(char contact[], char query[]) {
 }
 
 int main(int argc, char *argv[]) {
+    // Verifies correctness of arguments
+    // Checks for too many arguments and if search query is a number
     if (argc > 2) {
         fprintf(stderr, "Too many arguments\n");
-        return 0;
-    }
-
-    // Checks if the first argument is a number
-    if (argc > 1 && !isNumber(argv[1])) {
+        return EXIT_FAILURE;
+    } else if (argc > 1 && !isNumber(argv[1])) {
         fprintf(stderr, "Search query is not a valid number\n");
-        return 0;
+        return EXIT_FAILURE;
     }
 
     char c, contact[CONTACT_SIZE], name[LINE_SIZE], number[LINE_SIZE];
@@ -111,8 +115,5 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
-
-// TODO: Comments
-// TODO: Return codes
